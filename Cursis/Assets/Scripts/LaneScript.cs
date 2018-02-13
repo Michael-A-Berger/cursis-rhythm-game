a362fb16af0d4df6ee59a5ee7bd343459fde62e9
Michael-A-Berger <michael.anthony.berger@gmail.com>
2018-02-13 11:27:05 -0500

    Completed W3 Deliverable (Simple Pattern of Notes)
    NOTE: Timing is still a little off. Probably need to add unique offset for MP3 files.

---
+++ Cursis/Assets/Scripts/LaneScript.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaneScript : MonoBehaviour
+{
+	//Lane Properties
+    public Vector3 startPos;
+    public Vector3 lanePos;
+    public Vector3 endPos;
+    private List<float> noteTimes;
+    public GameObject notePrefab;
+    public Text display;
+    public string displayPrefix;
+    private List<Note> noteObjs;
+    private float beatsInAdvance = 2.0f;
+    private float noteSpeed = 1.0f;
+    private byte holdCounter = 0;
+	private LineRenderer hitLine;
+	private Color hitColor = Color.red;
+    private Color baseColor = Color.white;
+
+	void Start()
+	{
+        display.text = displayPrefix + "N/A";
+		hitLine = this.GetComponent<LineRenderer> ();
+        noteTimes = new List<float>();
+        noteObjs = new List<Note>();
+	}
+
+    //SetNotes()
+    public void SetNotes(List<float> notes)
+    {
+        noteTimes = notes;
+    }
+
+	//UpdateInput()
+	public void UpdateInput(bool input, float audioTime)
+	{
+        if (input)
+        {
+            if (holdCounter < 5)
+            {
+                hitLine.material.color = hitColor;
+                holdCounter++;
+            }
+            else
+            {
+                hitLine.material.color = baseColor;
+            }
+        }
+        else
+        {
+            hitLine.material.color = baseColor;
+            holdCounter = 0;
+        }
+
+        //Spawning New Notes
+        for (int num = 0; num < noteTimes.Count; num++)
+        {
+            float notePos = noteTimes[num];
+            
+            if (audioTime + beatsInAdvance > notePos)
+            {
+                Note newNote = Instantiate(notePrefab, startPos, Quaternion.identity).GetComponent<Note>();
+                newNote.SetProperties(notePos, noteSpeed, beatsInAdvance, startPos, lanePos, endPos);
+                noteObjs.Add(newNote);
+                noteTimes.Remove(notePos);
+                num--;
+            }
+        }
+
+        //Moving/Deleting the Note Objects
+        for (int num = 0; num < noteObjs.Count; num++)
+        {
+            bool destroyNote = false;
+            noteObjs[num].Move(audioTime);
+
+            if (noteObjs[num].lerpFactor >= 1.15f)
+            {
+                display.color = Color.red;
+                display.text = displayPrefix + "Miss!";
+                destroyNote = true;
+            }
+
+            if (holdCounter % 5 > 0   &&   Mathf.Abs(noteObjs[num].songPosition - audioTime) < 5f * (1f/60f))
+            {
+                display.color = Color.green;
+                display.text = displayPrefix + "Hit!";
+                destroyNote = true;
+            }
+
+            if (destroyNote)
+            {
+                Note copy = noteObjs[num];
+                noteObjs.Remove(copy);
+                Destroy(copy);
+                num--;
+            }
+        }
+	}
+
+
+}
--- Cursis/Assets/Scripts/TouchManager_Gameplay.cs
+++ Cursis/Assets/Scripts/TouchManager_Gameplay.cs
@@ -2,82 +2,167 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TouchManager_Gameplay : MonoBehaviour
 {
 	public GameObject hitOctagon;
-	private LineRenderer[] hitLines;
-	private List<Touch> touchPoints = new List<Touch>();
+    public Text inputText;
+	private LaneScript[] laneScripts;
+    private AudioSource song;
+    private float defaultNoteSpeed = 1.0f;
+    private int songBPM = 75;
+    private float crochet;
+    private float beatsInAdvance = 2.0f;
+    private float songStartTime = 0.0f;
 	private float measuredTime = 0.0f;
-	private Color hitColor = Color.red;
+	public bool debug;
 
 	void Start()
 	{
 		QualitySettings.vSyncCount = 0;
 		Application.targetFrameRate = 60;
 
-		hitLines = hitOctagon.GetComponentsInChildren<LineRenderer> ();
+        crochet = 60f / songBPM;
+
+        song = this.GetComponent<AudioSource>();
+        laneScripts = hitOctagon.GetComponentsInChildren<LaneScript> ();
+
+        createSongChart();
 	}
 
+    //FixedUpdate()
 	void FixedUpdate()
 	{
-		measuredTime = Time.realtimeSinceStartup;
-		
-		foreach (LineRenderer line in hitLines)
+        //Starting the song
+        if (songStartTime == 0.0 && Time.realtimeSinceStartup > 3)
+        {
+            song.Play();
+            songStartTime = (float) AudioSettings.dspTime;
+        }
+
+        //Updating the measured song time
+        if (songStartTime != 0.0)
+        {
+            measuredTime = (float) (AudioSettings.dspTime - songStartTime) * song.pitch;
+            Debug.Log("measuredTime: " + measuredTime);
+        }
+
+        //Defining the input list
+		List<bool> inputs = new List<bool>();
+		foreach (LaneScript lane in laneScripts)
+			inputs.Add(false);
+
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            debug = !debug;
+
+            if (debug)
+                inputText.text = "Input: Keyboard\n('S' to change)";
+            else
+                inputText.text = "Input: Touch\n('S' to change)";
+                
+        }
+
+		if (debug)
 		{
-			line.material.color = Color.white;
+            if (Input.GetKey(KeyCode.D))
+				inputs[0] = true;
+            if (Input.GetKey(KeyCode.E))
+				inputs[1] = true;
+            if (Input.GetKey(KeyCode.W))
+				inputs[2] = true;
+            if (Input.GetKey(KeyCode.Q))
+				inputs[3] = true;
+            if (Input.GetKey(KeyCode.A))
+				inputs[4] = true;
+            if (Input.GetKey(KeyCode.Z))
+				inputs[5] = true;
+            if (Input.GetKey(KeyCode.X))
+				inputs[6] = true;
+            if (Input.GetKey(KeyCode.C))
+				inputs[7] = true;
+		}
+		else
+		{
+			Vector3 screenPos = new Vector3(0, 0, 0);
+			Vector3 worldPos = new Vector3(0, 0, 0);
+			float rads = 0.0f;
+			foreach (Touch tch in Input.touches)
+			{
+				screenPos.x = tch.position.x;
+				screenPos.y = tch.position.y;
+				worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+
+                rads = Mathf.Atan2(worldPos.y, worldPos.x) + (Mathf.PI / 8);        //Add π/8 to offset the angle for flooring
+				rads = (rads + 2 * Mathf.PI) % (2 * Mathf.PI);
+                int sector = Mathf.FloorToInt(rads * (4 / Mathf.PI));
+                //Debug.Log("sector: " + sector);
+                inputs[sector] = true;
+			}
 		}
 
-		Vector3 screenPos = new Vector3 (0, 0, 0);
-		Vector3 worldPos = new Vector3 (0, 0, 0);
-		float rads = 0.0f;
-		float degrees = 0.0f;
-		foreach (Touch tch in Input.touches)
+        UpdateInput(inputs.ToArray(), measuredTime);
+	}
+
+    private void UpdateInput(bool[] inputs, float audioTime)
+	{
+		for (int num = 0; num < laneScripts.Length; num++)
 		{
-			screenPos.x = tch.position.x;
-			screenPos.y = tch.position.y;
-			worldPos = Camera.main.ScreenToWorldPoint (screenPos);
-
-			rads = Mathf.Atan2 (worldPos.y, worldPos.x);
-			int sector = Mathf.RoundToInt(rads * (4 / Mathf.PI));
-			Debug.Log ("Sector: " + sector);
-
-			switch (sector)
-			{
-				case 0:
-				hitLines [2].material.color = hitColor;
-					break;
-				case 1:
-				hitLines [1].material.color = hitColor;
-					break;
-				case 2:
-				hitLines [0].material.color = hitColor;
-					break;
-				case 3:
-				hitLines [7].material.color = hitColor;
-					break;
-				case 4:
-				case -4:
-					hitLines [6].material.color = hitColor;
-					break;
-				case -3:
-					hitLines [5].material.color = hitColor;
-					break;
-				case -2:
-					hitLines [4].material.color = hitColor;
-					break;
-				case -1:
-					hitLines [3].material.color = hitColor;
-					break;
-				default:
-					break;
-			}
-
-			/*
-			degrees = rads * (180 / Mathf.PI);
-			Debug.Log ("Degrees: " + Mathf.Round(degrees));
-			*/
+			laneScripts[num].UpdateInput(inputs[num], audioTime);
 		}
 	}
 
+    private void createSongChart()
+    {
+        List<float> rightNotes = new List<float>();
+        rightNotes.Add(crochet * 5f);
+        rightNotes.Add(crochet * 6f);
+        rightNotes.Add(crochet * 7f);
+        rightNotes.Add(crochet * 8f);
+        rightNotes.Add(crochet * 9f);
+        rightNotes.Add(crochet * 10f);
+        rightNotes.Add(crochet * 11f);
+        rightNotes.Add(crochet * 12f);
+        rightNotes.Add(crochet * 13f);
+        rightNotes.Add(crochet * 14f);
+        rightNotes.Add(crochet * 15f);
+        rightNotes.Add(crochet * 16f);
+        rightNotes.Add(crochet * 17f);
+        rightNotes.Add(crochet * 18f);
+        rightNotes.Add(crochet * 19f);
+        rightNotes.Add(crochet * 20f);
+        rightNotes.Add(crochet * 21f);
+        rightNotes.Add(crochet * 22f);
+        rightNotes.Add(crochet * 23f);
+        rightNotes.Add(crochet * 24f);
+        rightNotes.Add(crochet * 25f);
+
+        List<float> leftNotes = new List<float>();
+        leftNotes.Add(crochet * 5.5f);
+        leftNotes.Add(crochet * 6.5f);
+        leftNotes.Add(crochet * 7.5f);
+        leftNotes.Add(crochet * 8.5f);
+        leftNotes.Add(crochet * 9.5f);
+        leftNotes.Add(crochet * 10.5f);
+        leftNotes.Add(crochet * 11.5f);
+        leftNotes.Add(crochet * 12.5f);
+        leftNotes.Add(crochet * 13.5f);
+        leftNotes.Add(crochet * 14.5f);
+        leftNotes.Add(crochet * 15.5f);
+        leftNotes.Add(crochet * 16.5f);
+        leftNotes.Add(crochet * 17.5f);
+        leftNotes.Add(crochet * 18.5f);
+        leftNotes.Add(crochet * 19.5f);
+        leftNotes.Add(crochet * 20.5f);
+        leftNotes.Add(crochet * 21.5f);
+        leftNotes.Add(crochet * 22.5f);
+        leftNotes.Add(crochet * 23.5f);
+        leftNotes.Add(crochet * 24.5f);
+        leftNotes.Add(crochet * 25.5f);
+
+        laneScripts[0].SetNotes(rightNotes);
+        laneScripts[4].SetNotes(leftNotes);
+    }
 }
